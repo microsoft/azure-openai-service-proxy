f374baa67cdd71a6b4e1063d35b3e3d04c1e7a6a
Dave Glover <dglover@microsoft.com>
2023-10-07 22:24:00 +1100

    Input validation

--- src/endpoint/openai_proxy_rest/openai_async.py
+++ src/endpoint/openai_proxy_rest/openai_async.py
@@ -210,7 +210,7 @@ class OpenAIManager:
         logging.basicConfig(level=logging.WARNING)
         self.logger = logging.getLogger(__name__)
 
-    def _report_exception(
+    def __report_exception(
         self, message: str, http_status_code: int
     ) -> OpenAIChatResponse:
         """report exception"""
@@ -222,6 +222,68 @@ class OpenAIManager:
 
         return completion, http_status_code
 
+    def __validate_input(self, chat: OpenAIChatRequest):
+        """validate input"""
+        # do some basic input validation
+        if not chat.messages:
+            return self.__report_exception("Oops, no chat messages.", 400)
+
+        # search through the list of messages for a msg with a role of user
+        user_message = next(
+            (msg for msg in chat.messages if msg["role"] == "user"), None
+        )
+
+        if not user_message or user_message.get("content", "") == "":
+            return self.__report_exception(
+                "Oops, messages missing a user role message.", 400
+            )
+
+        # search through the list of messages for a msg with a role of system
+        system_message = next(
+            (msg for msg in chat.messages if msg["role"] == "system"), None
+        )
+
+        if not system_message or system_message.get("content", "") == "":
+            return self.__report_exception(
+                "Oops, messages missing a system role message.", 400
+            )
+
+        # check the max_tokens is between 1 and 4096
+        if not 1 <= chat.max_tokens <= 4096:
+            return self.__report_exception(
+                "Oops, max_tokens must be between 1 and 4096.", 400
+            )
+
+        # check the temperature is between 0 and 1
+        if not 0 <= chat.temperature <= 1:
+            return self.__report_exception(
+                "Oops, temperature must be between 0 and 1.", 400
+            )
+
+        # check the top_p is between 0 and 1
+        if not 0 <= chat.top_p <= 1:
+            return self.__report_exception("Oops, top_p must be between 0 and 1.", 400)
+
+        # check the frequency_penalty is between 0 and 1
+        if not 0 <= chat.frequency_penalty <= 1:
+            return self.__report_exception(
+                "Oops, frequency_penalty must be between 0 and 1.", 400
+            )
+
+        # check the presence_penalty is between 0 and 1
+        if not 0 <= chat.presence_penalty <= 1:
+            return self.__report_exception(
+                "Oops, presence_penalty must be between 0 and 1.", 400
+            )
+
+        # check stop sequence are printable characters
+        if not chat.stop_sequence.isprintable():
+            return self.__report_exception(
+                "Oops, stop_sequence must be printable characters.", 400
+            )
+
+        return None, None
+
     @retry(
         wait=wait_random_exponential(min=4, max=10),
         stop=stop_after_attempt(2),
@@ -229,6 +291,10 @@ class OpenAIManager:
     )
     async def call_openai_chat(self, chat: OpenAIChatRequest) -> OpenAIChatResponse:
         """call openai with retry"""
+
+        completion, http_status_code = self.__validate_input(chat)
+        if completion or http_status_code:
+            return completion, http_status_code
 
         completion = OpenAIChatResponse.empty()
 
@@ -279,31 +345,31 @@ class OpenAIManager:
 
         except openai.error.InvalidRequestError as invalid_request_exception:
             # this exception captures content policy violation policy
-            return self._report_exception(
+            return self.__report_exception(
                 str(invalid_request_exception.user_message),
                 invalid_request_exception.http_status,
             )
 
         except openai.error.RateLimitError as rate_limit_exception:
-            return self._report_exception(
+            return self.__report_exception(
                 "Oops, OpenAI rate limited. Please try again.",
                 rate_limit_exception.http_status,
             )
 
         except openai.error.ServiceUnavailableError as service_unavailable_exception:
-            return self._report_exception(
+            return self.__report_exception(
                 "Oops, OpenAI unavailable. Please try again.",
                 service_unavailable_exception.http_status,
             )
 
         except openai.error.Timeout as timeout_exception:
-            return self._report_exception(
+            return self.__report_exception(
                 "Oops, OpenAI timeout. Please try again.",
                 timeout_exception.http_status,
             )
 
         except openai.error.OpenAIError as openai_error_exception:
-            return self._report_exception(
+            return self.__report_exception(
                 str(openai_error_exception.user_message),
                 openai_error_exception.http_status,
             )
