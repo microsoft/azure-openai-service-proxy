b64d7666b65d07a77d84d6c6c61a5fa2be776da2
Dave Glover <dglover@microsoft.com>
2023-11-14 22:22:14 +1100

    examples with docs link

--- src/examples/python/openai_sdk_1.x/openai_chat.py
+++ src/examples/python/openai_sdk_1.x/openai_chat.py
@@ -1,5 +1,7 @@
 """ Test script for OpenAI chat API """
 
+# See documentation at https://gloveboxes.github.io/azure-openai-service-proxy/category/developer-endpoints/
+
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
