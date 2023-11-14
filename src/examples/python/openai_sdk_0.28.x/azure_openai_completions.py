7e769ccfb51a0fdeda5b0070367140319ba30e3e
Dave Glover <dglover@microsoft.com>
2023-11-14 17:37:27 +1100

    completions examples

---
+++ src/examples/python/openai_sdk_0.28.x/azure_openai_completions.py
@@ -0,0 +1,25 @@
+''' Test completions with azure openai '''
+
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+
+ENDPOINT_URL = os.environ.get("ENDPOINT_URL")
+API_KEY = os.environ.get("API_KEY")
+API_VERSION = "2023-09-01-preview"
+DEPLOYMENT_NAME = "davinci-002"
+ENGINE_NAME = "text-davinci-002-prod"
+
+openai.api_type = "azure"
+openai.api_key = API_KEY
+openai.api_base = ENDPOINT_URL
+openai.api_version = API_VERSION
+
+
+response = openai.Completion.create(
+    engine=ENGINE_NAME, prompt="This is a test", max_tokens=5
+)
+
+print(response)
