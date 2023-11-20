1579ea7358486c73248e95117a6de721b9043911
Dave Glover <dglover@microsoft.com>
2023-11-20 13:11:36 +1100

    Streaming Chat Completions support
    - Streaming enabled for Chat Completions
    - Python and .NET Examples in the examples folder

--- src/endpoint/simple-fastapi-container/src/api/openai_async.py
+++ src/endpoint/simple-fastapi-container/src/api/openai_async.py
@@ -3,7 +3,7 @@
 import json
 import logging
 import time
-from typing import Tuple
+from typing import Tuple, AsyncGenerator
 from fastapi import HTTPException
 
 import httpx
@@ -184,6 +184,60 @@ class OpenAIAsyncManager:
 
             raise HTTPException(
                 status_code=http_status_error.response.status_code, detail=detail
+            ) from http_status_error
+
+        except httpx.ConnectError as exc:
+            raise HTTPException(
+                status_code=504,
+                detail="Service connection error.",
+            ) from exc
+
+        except httpx.ConnectTimeout as exc:
+            raise HTTPException(
+                status_code=504,
+                detail="Service connection timeout error.",
+            ) from exc
+
+        except Exception as exc:
+            raise HTTPException(
+                status_code=500,
+                detail="OpenAI API call failed",
+            ) from exc
+
+    async def async_post_streaming(
+        self,
+        openai_request: str,
+        url: str,
+    ) -> AsyncGenerator:
+        """async rest post"""
+
+        headers = {
+            "Content-Type": "application/json",
+            "api-key": self.deployment.endpoint_key,
+        }
+
+        try:
+
+            async def streaming_post_request():
+                async with httpx.AsyncClient() as client:
+                    async with client.stream(
+                        "POST",
+                        url=url,
+                        headers=headers,
+                        json=openai_request,
+                        timeout=HTTPX_TIMEOUT_SECONDS,
+                    ) as response:
+                        async for chunk in response.aiter_bytes():
+                            yield chunk
+
+            return streaming_post_request()
+
+        except httpx.HTTPStatusError as http_status_error:
+            raise HTTPException(
+                status_code=http_status_error.response.status_code,
+                detail=json.loads(http_status_error.response.text)
+                .get("error")
+                .get("message", "OpenAI Error"),
             ) from http_status_error
 
         except httpx.ConnectError as exc:
---
+++ src/examples/python/openai_sdk_1.x/azure_openai_chat_streaming.py
@@ -0,0 +1,51 @@
+""" Test Azure OpenAI Chat Completions Stream API """
+
+import os
+import time
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+ENDPOINT_URL = os.environ.get("ENDPOINT_URL")
+API_KEY = os.environ.get("API_KEY")
+API_VERSION = "2023-09-01-preview"
+MODEL_NAME = "text-davinci-002"
+
+
+client = OpenAI(
+    base_url=ENDPOINT_URL,
+    api_key=API_KEY,
+    # api_version=API_VERSION,
+)
+
+MESSAGES = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {
+        "role": "assistant",
+        "content": "The Los Angeles Dodgers won the World Series in 2020.",
+    },
+    {"role": "user", "content": "Where was it played?"},
+]
+
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the meaning of life!"},
+    ],
+    stream=True,
+)
+
+
+for chunk in response:
+    if chunk.choices and len(chunk.choices) > 0:
+        content = chunk.choices[0].delta.content
+        if content:
+            print(content, end="")
+        # delay to simulate real-time chat
+        time.sleep(0.05)
+
+print()
