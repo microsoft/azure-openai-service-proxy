f6ee559a8b06ce9b65ab0356839eaddbd9115e60
Dave Glover <dglover@microsoft.com>
2023-11-28 15:20:44 +1100

    Azure OpenAI Chat Extensions Bring Your Own Data
    Added bring your own dataSource extension for Chat Completions
    Added Python and .NET Streaming and Non streaming examples

--- src/endpoint/simple-fastapi-container/src/api/chat_completions.py
+++ src/endpoint/simple-fastapi-container/src/api/chat_completions.py
@@ -13,6 +13,7 @@ from .configuration import OpenAIConfig
 from .openai_async import OpenAIAsyncManager
 
 OPENAI_CHAT_COMPLETIONS_API_VERSION = "2023-09-01-preview"
+OPENAI_CHAT_COMPLETIONS_EXTENSIONS_API_VERSION = "2023-08-01-preview"
 
 logging.basicConfig(level=logging.WARNING)
 
@@ -21,16 +22,18 @@ class ChatCompletionsRequest(BaseModel):
     """OpenAI Chat Request"""
 
     messages: list[dict[str, str]]
+    dataSources: list[Any] | None = None
     max_tokens: int = None
     temperature: float = None
+    n: int | None = None
     stream: bool = False
     top_p: float | None = None
-    stop_sequence: Any | None = None
-    frequency_penalty: float = 0
-    presence_penalty: float = 0
+    stop: str | list[str] | None = None
+    frequency_penalty: float | None = None
+    presence_penalty: float | None = None
     functions: list[dict[str, Any]] | None = None
-    function_call: str | dict[str, str] = "auto"
-    api_version: str = OPENAI_CHAT_COMPLETIONS_API_VERSION
+    function_call: str | dict[str, str] | None = None
+    api_version: str | None = None
 
 
 class ChatCompletions:
@@ -51,41 +54,36 @@ class ChatCompletions:
     def validate_input(self, chat: ChatCompletionsRequest):
         """validate input"""
         # do some basic input validation
-        if not chat.messages:
-            self.__throw_validation_error("Oops, no chat messages.", 400)
 
         # check the max_tokens is between 1 and 4096
-        if chat.max_tokens and not 1 <= chat.max_tokens <= 4096:
+        if chat.max_tokens is not None and not 1 <= chat.max_tokens <= 4096:
             self.__throw_validation_error(
                 "Oops, max_tokens must be between 1 and 4096.", 400
             )
 
+        if chat.n is not None and not 1 <= chat.n <= 10:
+            self.__throw_validation_error("Oops, n must be between 1 and 10.", 400)
+
         # check the temperature is between 0 and 1
-        if chat.temperature and not 0 <= chat.temperature <= 1:
+        if chat.temperature is not None and not 0 <= chat.temperature <= 1:
             self.__throw_validation_error(
                 "Oops, temperature must be between 0 and 1.", 400
             )
 
         # check the top_p is between 0 and 1
-        if chat.top_p and not 0 <= chat.top_p <= 1:
+        if chat.top_p is not None and not 0 <= chat.top_p <= 1:
             self.__throw_validation_error("Oops, top_p must be between 0 and 1.", 400)
 
         # check the frequency_penalty is between 0 and 1
-        if chat.frequency_penalty and not 0 <= chat.frequency_penalty <= 1:
+        if chat.frequency_penalty is not None and not 0 <= chat.frequency_penalty <= 1:
             self.__throw_validation_error(
                 "Oops, frequency_penalty must be between 0 and 1.", 400
             )
 
         # check the presence_penalty is between 0 and 1
-        if chat.presence_penalty and not 0 <= chat.presence_penalty <= 1:
+        if chat.presence_penalty is not None and not 0 <= chat.presence_penalty <= 1:
             self.__throw_validation_error(
                 "Oops, presence_penalty must be between 0 and 1.", 400
-            )
-
-        # check stop sequence are printable characters
-        if chat.stop_sequence and not chat.stop_sequence.isprintable():
-            self.__throw_validation_error(
-                "Oops, stop_sequence must be printable characters.", 400
             )
 
     async def call_openai_chat_completion(
@@ -98,34 +96,29 @@ class ChatCompletions:
 
         deployment = await self.openai_config.get_deployment()
 
-        if chat.functions:
-            # https://platform.openai.com/docs/guides/gpt/function-calling
-            # function_call can be = none, auto, or {"name": "<insert-function-name>"}
-            openai_request = {
-                "messages": chat.messages,
-                "max_tokens": chat.max_tokens,
-                "temperature": chat.temperature,
-                "functions": chat.functions,
-                "function_call": chat.function_call,
-            }
-
+        # if dataSources are provided, use the extensions API
+        if chat.dataSources:
+            api_version = (
+                chat.api_version or OPENAI_CHAT_COMPLETIONS_EXTENSIONS_API_VERSION
+            )
+            url = (
+                f"https://{deployment.resource_name}.openai.azure.com/openai/deployments/"
+                f"{deployment.deployment_name}/extensions/chat/completions"
+                f"?api-version={api_version}"
+            )
         else:
-            openai_request = {
-                "messages": chat.messages,
-                "max_tokens": chat.max_tokens,
-                "temperature": chat.temperature,
-                "stream": chat.stream,
-                "top_p": chat.top_p,
-                "stop": chat.stop_sequence,
-                "frequency_penalty": chat.frequency_penalty,
-                "presence_penalty": chat.presence_penalty,
-            }
+            api_version = chat.api_version or OPENAI_CHAT_COMPLETIONS_API_VERSION
+            url = (
+                f"https://{deployment.resource_name}.openai.azure.com/openai/deployments/"
+                f"{deployment.deployment_name}/chat/completions"
+                f"?api-version={api_version}"
+            )
 
-        url = (
-            f"https://{deployment.resource_name}.openai.azure.com/openai/deployments/"
-            f"{deployment.deployment_name}/chat/completions"
-            f"?api-version={chat.api_version}"
-        )
+        openai_request = {}
+
+        for key, value in chat.__dict__.items():
+            if value is not None and key != "api_version":
+                openai_request[key] = value
 
         async_mgr = OpenAIAsyncManager(deployment)
 
---
+++ src/examples/dotnet/azure_chat_completions_your_data/README.md
@@ -0,0 +1,12 @@
+# Notes
+
+## Nuget Packages
+
+```shell
+dotnet add package Azure.AI.OpenAI --prerelease
+dotnet add package DotNetEnv
+```
+
+## YOUR_EVENT_AUTH_TOKEN
+
+YOUR_EVENT_AUTH_TOKEN is the API key and consists of your EVENT_CODE/GITHUB_USERNAME
