3e3d5fd2257b61a533e0723742e6d17ca266203b
Dave Glover <dglover@microsoft.com>
2023-12-21 14:20:01 +1100

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,15 +11,15 @@ nvm install
 npm i
 npm install -g @azure/static-web-apps-cli
 
-echo Setting up .NET environment...
-cd ../
-dotnet tool restore
-dotnet restore
+# echo Setting up .NET environment...
+# cd ../
+# dotnet tool restore
+# dotnet restore
 
-echo Setting up database...
-# Per: https://learn.microsoft.com/en-gb/sql/tools/sqlcmd/sqlcmd-utility?view=sql-server-ver16&tabs=go%2Clinux&pivots=cs1-bash#download-and-install-sqlcmd
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
+# sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -i ../database/schema.sql
+
+# install postgresql client
 sudo apt-get update
-sudo apt-get install sqlcmd
-sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -i ../database/schema.sql
+sudo apt install postgresql-client -y
+
+psql -U admin -d aoai-proxy -h localhost -W -f ../database/aoai-proxy.sql
