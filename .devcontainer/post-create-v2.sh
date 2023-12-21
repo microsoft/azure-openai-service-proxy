548019d9408f616f39b5083d5888172680256ede
Dave Glover <dglover@microsoft.com>
2023-12-21 15:47:59 +1100

    updated build files

--- .devcontainer/post-create-v2.sh
+++ .devcontainer/post-create-v2.sh
@@ -1,4 +1,14 @@
+
+echo setting up postgresql...
+
+# install postgresql client
+sudo apt-get update
+sudo apt install postgresql-client -y
+
+psql -U admin -d aoai-proxy -h localhost -w -f ./database/aoai-proxy.sql
+
 echo Setting up Python environment...
+
 python3 -m pip install -r requirements-dev.txt
 
 echo Setting up commit hooks...
@@ -11,15 +21,8 @@ nvm install
 npm i
 npm install -g @azure/static-web-apps-cli
 
-# echo Setting up .NET environment...
-# cd ../
-# dotnet tool restore
-# dotnet restore
 
 # sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -i ../database/schema.sql
 
-# install postgresql client
-sudo apt-get update
-sudo apt install postgresql-client -y
 
-psql -U admin -d aoai-proxy -h localhost -W -f ../database/aoai-proxy.sql
+
