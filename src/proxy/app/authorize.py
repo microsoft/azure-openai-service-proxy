635d3f6f749de776afbd8e581310c7bcd541f463
Dave Glover <dglover@microsoft.com>
2023-12-22 10:34:48 +0000

    clean up

--- src/proxy/app/authorize.py
+++ src/proxy/app/authorize.py
@@ -11,11 +11,7 @@ from fastapi import HTTPException
 from .lru_cache_with_expiry import lru_cache_with_expiry
 from .monitor import Monitor, MonitorEntity
 
-USER_TABLE_NAME = "user"
-EVENT_TABLE_NAME = "event"
-
 MAX_AUTH_TOKEN_LENGTH = 40
-MANAGEMENT_TABLE_NAME = "management"
 
 logging.basicConfig(level=logging.WARNING)
 
@@ -33,13 +29,13 @@ class Authorize:
         self.monitor = Monitor(connection_string=connection_string)
         self.logging = logging.getLogger(__name__)
 
-    async def __is_user_authorized(self, event_id: str, api_key: UUID, deployment_name: str) -> AuthorizeResponse:
+    async def __is_user_authorized(self, event_code: str, api_key: UUID, deployment_name: str) -> AuthorizeResponse:
         """Check if user is authorized"""
 
         try:
             conn = await self.db_manager.get_connection()
 
-            result = await conn.fetchrow("SELECT * from aoai.get_attendee_authorized($1, $2)", event_id, api_key)
+            result = await conn.fetchrow("SELECT * from aoai.get_attendee_authorized($1, $2)", event_code, api_key)
 
             if result is None or len(result) == 0:
                 raise HTTPException(
@@ -47,23 +43,11 @@ class Authorize:
                     detail="Authentication failed.",
                 )
 
-            authorize_response = AuthorizeResponse(
-                is_authorized=True,
-                max_token_cap=result.get("max_token_cap"),
-                daily_request_cap=result.get("daily_request_cap"),
-                user_id=result.get("user_id"),
-                event_id=result.get("event_id"),
-                event_code=result.get("event_code"),
-                user_token=api_key,
-                event_name=result.get("event_code"),
-                event_url=result.get("event_url"),
-                event_url_text=result.get("event_url_text"),
-                organizer_name=result.get("organizer_name"),
-                organizer_email=result.get("organizer_email"),
-                deployment_name=deployment_name,
-            )
+            result_dict = dict(result)
+            result_dict["is_authorized"] = True
+            result_dict["deployment_name"] = deployment_name
 
-            return authorize_response
+            return AuthorizeResponse(**result_dict)
 
         except HTTPException:
             raise
@@ -94,13 +78,10 @@ class Authorize:
                 detail="Authentication failed.",
             )
 
-        event_id = id_parts[0].strip()
-        user_id = id_parts[1].strip()
+        event_code = id_parts[0].strip()
+        api_key = id_parts[1].strip()
 
-        # throw a not Authentication failed exception if
-        # the event_code len is less than 6 or greater than MAX_AUTH_TOKEN_LENGTH
-
-        if not 6 <= len(event_id) <= MAX_AUTH_TOKEN_LENGTH:
+        if not 4 <= len(event_code) <= MAX_AUTH_TOKEN_LENGTH:
             raise HTTPException(
                 status_code=401,
                 detail="Authentication failed.",
@@ -108,35 +89,28 @@ class Authorize:
 
         # is the user_id a valid guid
         try:
-            user_id = UUID(user_id)
+            api_key = UUID(api_key)
         except ValueError as exc:
             raise HTTPException(
                 status_code=401,
                 detail="Authentication failed.",
             ) from exc
 
-        # check event code does not user any of the azure table reserved characters for row key
-        if any(c in event_id for c in ["\\", "/", "#", "?", "\t", "\n", "\r"]):
-            raise HTTPException(
-                status_code=401,
-                detail="Authentication failed.",
-            )
-
         # check event code is only printable characters
-        if not all(c in string.printable for c in event_id):
+        if not all(c in string.printable for c in event_code):
             raise HTTPException(
                 status_code=401,
                 detail="Authentication failed.",
             )
 
         authorize_response = await self.__is_user_authorized(
-            event_id=event_id, api_key=user_id, deployment_name=deployment_name
+            event_code=event_code, api_key=api_key, deployment_name=deployment_name
         )
 
         return authorize_response
 
-    async def __authorize_azure_api_access(self, *, headers, deployment_name: str) -> AuthorizeResponse:
-        """validate azure sdk formatted API request"""
+    async def authorize_azure_api_access(self, *, headers: str, deployment_name: str) -> AuthorizeResponse:
+        """authorize api access"""
 
         if "api-key" not in headers:
             raise HTTPException(
@@ -147,8 +121,3 @@ class Authorize:
         access_token = headers.get("api-key")
 
         return await self.__authorize(access_token=access_token, deployment_name=deployment_name)
-
-    async def authorize_api_access(self, *, headers: str, deployment_name: str) -> AuthorizeResponse:
-        """authorize api access"""
-
-        return await self.__authorize_azure_api_access(headers=headers, deployment_name=deployment_name)
