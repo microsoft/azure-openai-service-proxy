c12e95f4d1e0cee56b0a2ffc8f0348d3b4bc44a1
Dave Glover <dglover@microsoft.com>
2023-12-23 13:03:50 +1100

    Update aoai-proxy.sql

--- database/aoai-proxy.sql
+++ database/aoai-proxy.sql
@@ -26,20 +26,6 @@ CREATE SCHEMA aoai;
 ALTER SCHEMA aoai OWNER TO admin;
 
 --
--- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
-
-
---
--- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner:
---
-
-COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
-
-
---
 -- Name: model_type; Type: TYPE; Schema: aoai; Owner: admin
 --
 
