938b9f6a9abda8fd2ffde20b605d8e4c8b882b61
Aaron Powell <me@aaron-powell.com>
2024-01-05 16:50:29 +1100

    CRUD for events (#76)
    * Moving files back to match the standard pattern of Blazor apps
    
    * Moving to mudblazor for UI framework
    
    * Tidying up how we register auth
    
    * New base layout with mudblazor
    
    * Overhaul of the list page with Mud
    
    * Scaffold of the create screen
    
    * remove sql statement from bash script
    
    * Fixing the add_event function and extending the Owner table to have more info
    
    * tasks from the c# extension
    
    * Implementing the create event flow
    
    * Removing column that isn't that useful
    
    * Refactoring the event editor form to a reusable component
    
    This will make the Edit Event screen easier
    
    * Edit event workflow started

---
+++ src/AzureOpenAIProxy.Management/Components/EventManagement/EventEditor.razor.cs
@@ -0,0 +1,90 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace AzureOpenAIProxy.Management.Components.EventManagement;
+
+public partial class EventEditor : ComponentBase
+{
+    [Parameter]
+    public EventEditorModel Model { get; set; } = null!;
+
+    [Parameter]
+    public EventCallback<EventEditorModel> ModelChanged { get; set; }
+
+    [Parameter]
+    public EventCallback<EventEditorModel> OnValidSubmit { get; set; }
+
+    private EditContext? editContext;
+
+    private ValidationMessageStore? messageStore;
+
+    private bool isSubmitting = false;
+
+    protected override Task OnInitializedAsync()
+    {
+        Model ??= new();
+        editContext = new(Model);
+        editContext.OnValidationRequested += EditContext_OnValidationRequested;
+        messageStore = new(editContext);
+        return Task.CompletedTask;
+    }
+
+    private void EditContext_OnValidationRequested(object? sender, ValidationRequestedEventArgs e)
+    {
+        if (Model is not null && editContext is not null && messageStore is not null)
+        {
+            if (Model.Start > Model.End)
+            {
+                messageStore.Add(editContext.Field(nameof(Model.Start)), "Start date must be before end date");
+                messageStore.Add(editContext.Field(nameof(Model.End)), "End date must be after start date");
+            }
+        }
+    }
+
+    public async Task HandleValidSubmit()
+    {
+        if (Model is null)
+        {
+            return;
+        }
+
+        isSubmitting = true;
+        await OnValidSubmit.InvokeAsync(Model);
+        isSubmitting = false;
+    }
+
+    public class EventEditorModel
+    {
+        [Required]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Url text is required")]
+        [StringLength(256)]
+        public string? UrlText { get; set; }
+        [Required(ErrorMessage = "Url is required")]
+        [StringLength(256)]
+        [Url]
+        public string? Url { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public DateTime? Start { get; set; }
+        [Required]
+        public DateTime? End { get; set; }
+        [Required(ErrorMessage = "Organizer name is required")]
+        [StringLength(128)]
+        public string? OrganizerName { get; set; }
+        [Required(ErrorMessage = "Organizer email is required")]
+        [StringLength(128)]
+        [EmailAddress]
+        public string? OrganizerEmail { get; set; }
+        [Required(ErrorMessage = "Specify the maximum number of tokens allowed per request")]
+        public int MaxTokenCap { get; set; } = 4096;
+        [Required]
+        public bool SingleCode { get; set; }
+        [Required(ErrorMessage = "Specify the maximum number of requests allowed per day per token")]
+        public int DailyRequestCap { get; set; } = 10000;
+        public bool Active { get; set; }
+    }
+}
---
+++ src/AzureOpenAIProxy.Management/Components/Pages/EventAdd.razor.cs
@@ -0,0 +1,42 @@
+using AzureOpenAIProxy.Management.Components.EventManagement;
+using AzureOpenAIProxy.Management.Database;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace AzureOpenAIProxy.Management.Components.Pages;
+
+public partial class EventAdd : ComponentBase
+{
+    [Inject]
+    public required AoaiProxyContext DbContext { get; set; }
+
+    [Inject]
+    public required NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    public required AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+    public async Task HandleValidSubmit(EventEditor.EventEditorModel model)
+    {
+        Event evt = new()
+        {
+            EventCode = model.Name!,
+            EventUrlText = model.UrlText!,
+            EventUrl = model.Url!,
+            EventMarkdown = model.Description!,
+            StartUtc = model.Start!.Value,
+            EndUtc = model.End!.Value,
+            OrganizerName = model.OrganizerName!,
+            OrganizerEmail = model.OrganizerEmail!,
+            MaxTokenCap = model.MaxTokenCap,
+            SingleCode = model.SingleCode,
+            DailyRequestCap = model.DailyRequestCap,
+            Active = model.Active
+        };
+
+        AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+
+        await DbContext.CreateEventAsync(evt, authState.User.GetEntraId());
+        NavigationManager.NavigateTo("/events", forceLoad: true);
+    }
+}
