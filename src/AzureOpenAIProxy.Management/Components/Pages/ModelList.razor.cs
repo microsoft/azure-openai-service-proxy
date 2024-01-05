bb0bb8766782d35ddc2fddaf070f84ca2bb8a389
Aaron Powell <me@aaron-powell.com>
2024-01-05 09:32:08 +0000

    Creating a new model works

--- src/AzureOpenAIProxy.Management/Components/Pages/ModelList.razor.cs
+++ src/AzureOpenAIProxy.Management/Components/Pages/ModelList.razor.cs
@@ -1,7 +1,24 @@
+using AzureOpenAIProxy.Management.Database;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace AzureOpenAIProxy.Management.Components.Pages;
 
-public partial class ModelList : ComponentBase {
+public partial class ModelList : ComponentBase
+{
+    [Inject]
+    public required AoaiProxyContext DbContext { get; set; }
 
+    [Inject]
+    public required AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+    public IEnumerable<OwnerCatalog>? Models { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        AuthenticationState authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        string entraId = authState.User.GetEntraId();
+        Models = await DbContext.OwnerCatalogs.Where(e => e.Owner.EntraId == entraId).ToListAsync();
+    }
 }
