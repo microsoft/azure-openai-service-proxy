09804ca680c0057dab6325d13ca9dd5c4a999f0c
Aaron Powell <me@aaron-powell.com>
2024-01-22 23:47:44 +0000

    Script will register app and the redirect uris

--- infra/scripts/app-registration.sh
+++ infra/scripts/app-registration.sh
@@ -24,30 +24,51 @@ else
 
     app_id=$(echo $app_registrations | jq -r '.[0].appId')
 fi
+tenantId=$(az account show | jq -r '.tenantId')
 
-echo "AzureAD__ClientId:$app_id"
-echo "AzureAD__TenantId:$(az account show | jq -r '.tenantId')"
+if [ $ENVIRONMENT == 'development' ]; then
+    echo Update the appsettings.Development.json file with the following values:
+    echo "    \"AzureAd\": {"
+    echo "        \"TenantId\": \"$tenantId\","
+    echo "        \"ClientId\": \"$app_id\""
+    echo "    }"
+fi
+
+echo Adding environment variables to azd environment
+azd env set AUTH_TENANT_ID $tenantId
+azd env set AUTH_CLIENT_ID $app_id
 
 echo "Ensuring admin app is registered for $app_name"
 
 app_registration=$(az ad app show --id $app_id -o json)
+existing_redirects=$(echo $app_registration | jq '.web.redirectUris')
 
 if [ $ENVIRONMENT == 'development' ]; then
     echo Ensuring localhost is registered as a redirect uri
-    if $(echo $app_registration | jq '.web.redirectUris' | jq 'contains(["http://localhost:5175$signin_path"])'); then
+    if $(echo $existing_redirects | jq "contains([\"http://localhost:5175${signin_path}\"])"); then
         echo "http://localhost:5175$signin_path already registered as a redirect uri"
     else
         echo "Registering http://localhost:5175$signin_path as a redirect uri"
-        az ad app update --id $app_id --web-redirect-uris "http://localhost:5175$signin_path"
+        az ad app update --id $app_id --web-redirect-uris $(echo $existing_redirects | jq -r 'join(" ")') "http://localhost:5175${signin_path}"
+
+        # Refresh app registration and redirect uris
+        app_registration=$(az ad app show --id $app_id -o json)
+        existing_redirects=$(echo $app_registration | jq '.web.redirectUris')
     fi
 fi
 
 if [ $SERVICE_ADMIN_URI ]; then
     echo Ensuring $SERVICE_ADMIN_URI is registered as a redirect uri
-    if $(echo $app_registration | jq '.web.redirectUris' | jq 'contains(["$SERVICE_ADMIN_URI$signin_path"])'); then
+    if $(echo $existing_redirects | jq "contains([\"${SERVICE_ADMIN_URI}${signin_path}\"])"); then
         echo "$SERVICE_ADMIN_URI$signin_path already registered as a redirect uri"
     else
         echo "Registering $SERVICE_ADMIN_URI$signin_path as a redirect uri"
-        az ad app update --id $app_id --web-redirect-uris "$SERVICE_ADMIN_URI$signin_path"
+        az ad app update --id $app_id --web-redirect-uris $(echo $existing_redirects | jq -r 'join(" ")') "${SERVICE_ADMIN_URI}${signin_path}"
+
+        # Refresh app registration and redirect uris
+        app_registration=$(az ad app show --id $app_id -o json)
+        existing_redirects=$(echo $app_registration | jq '.web.redirectUris')
     fi
 fi
+
+echo App Registration complete
