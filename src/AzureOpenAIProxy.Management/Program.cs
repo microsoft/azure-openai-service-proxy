5b0877640fde3e31fd10539f6ecef20b9a661e5c
Dave Glover <dglover@microsoft.com>
2024-02-22 13:57:29 +1100

    Update Program.cs
    reenable conditional is dev environment

--- src/AzureOpenAIProxy.Management/Program.cs
+++ src/AzureOpenAIProxy.Management/Program.cs
@@ -35,12 +35,12 @@ builder.Services.AddApplicationInsightsTelemetry();
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-// }
+}
 
 app.UseHttpsRedirection();
 
