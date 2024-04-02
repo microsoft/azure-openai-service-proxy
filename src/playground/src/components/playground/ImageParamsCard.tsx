81e4df6b6bcb79be107b4cb1c331048b9852f6b7
Aaron Powell <me@aaron-powell.com>
2024-04-02 05:36:40 +0000

    Using new input component on image screen

--- src/playground/src/components/playground/ImageParamsCard.tsx
+++ src/playground/src/components/playground/ImageParamsCard.tsx
@@ -1,35 +1,15 @@
-import { Select, makeStyles, Label, Tooltip } from "@fluentui/react-components";
+import { makeStyles } from "@fluentui/react-components";
 import { Card } from "./Card";
-// import { DividerBlock } from "./DividerBlock";
-// import { ParamInput } from "./ParamInput";
 import { GetImagesOptions } from "@azure/openai";
 import { useEventDataContext } from "../../providers/EventDataProvider";
-// import { ParamInputLabel } from "./ParamInputLabel";
-import { Info16Filled } from "@fluentui/react-icons";
+import { ParamSelect } from "./ParamSelect";
 
 const useStyles = makeStyles({
-  input: {
-    fontSize: "medium",
-    marginLeft: "0px",
-    width: "100%",
-    textAlign: "left",
-    height: "auto",
-  },
   container: {
     marginTop: "0px",
     marginBottom: "24px",
   },
-  label: {
-    fontSize: "medium",
-    marginBottom: "0px",
-    marginTop: "0px",
-    textAlign: "justify",
-    display: "block",
-    fontWeight: "bold",
-  },
-  tooltip: {
-    marginLeft: "6px",
-  },
+
   body: {
     paddingLeft: "15px",
     paddingRight: "15px",
@@ -37,7 +17,7 @@ const useStyles = makeStyles({
     marginRight: "0px",
     marginBottom: "0px",
     marginLeft: "0px",
-  }
+  },
 });
 
 type ImageParamsCardProps = {
@@ -45,49 +25,29 @@ type ImageParamsCardProps = {
     label: keyof GetImagesOptions | "model",
     newValue: number | string
   ) => void;
-
   settings: GetImagesOptions;
 };
 
-export const ImageParamsCard = ({
-  updateSettings,
-  // settings,
-}: ImageParamsCardProps) => {
-  // const selectId = useId("size");
+export const ImageParamsCard = ({ updateSettings }: ImageParamsCardProps) => {
   const { eventData, isAuthorized } = useEventDataContext();
   const styles = useStyles();
 
   return (
     <div className={styles.body}>
       <Card header="Configuration">
-
-          <Label className={styles.label} htmlFor="ModelLabel" style={{ marginBottom: "0px", paddingBottom: "0px" }}>
-            Model
-            <Tooltip content="Select the model to use for the AI chat. The model determines the type of responses the AI will generate. Different models have different capabilities and are trained on different types of data."
-              relationship="description" >
-              <Info16Filled className={styles.tooltip} />
-            </Tooltip>
-          </Label>
-
-          <Select
-            id="capabilities"
-            style={{ marginTop: "0px", marginBottom: "0px" }}
-            disabled={!isAuthorized}
-            onChange={(e) => {
-              const newValue = e.currentTarget.value;
-              updateSettings("model", newValue);
-            }}
-          >
-            <option value="">Select a model</option>
-            {eventData &&
+        <ParamSelect
+          label="Model"
+          explain="Select the model to use for the AI chat. The model determines the type of responses the AI will generate. Different models have different capabilities and are trained on different types of data."
+          disabled={!isAuthorized}
+          defaultOption="Select a model"
+          options={
+            (eventData &&
               eventData.capabilities["openai-dalle3"] &&
-              eventData.capabilities["openai-dalle3"].map((model) => (
-                <option key={model} value={model}>
-                  {model}
-                </option>
-              ))}
-          </Select>
-
+              eventData.capabilities["openai-dalle3"]) ||
+            []
+          }
+          onUpdate={(newValue) => updateSettings("model", newValue)}
+        />
       </Card>
     </div>
   );
