c1c208696c6947edf745f827981bf5b21c291961
Dave Glover <dglover@microsoft.com>
2024-05-03 20:22:35 +1000

    working chart :)

--- src/AzureOpenAIProxy.Management/Components/Pages/EventMetrics.razor.cs
+++ src/AzureOpenAIProxy.Management/Components/Pages/EventMetrics.razor.cs
@@ -1,6 +1,7 @@
 using AzureOpenAIProxy.Management.Database;
 using AzureOpenAIProxy.Management.Services;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace AzureOpenAIProxy.Management.Components.Pages;
 
@@ -12,6 +13,12 @@ public partial class EventMetrics
     [Parameter]
     public string EventId { get; set; } = null!;
 
+    private List<ChartSeries> ChartSeries { get; set; } = [];
+
+    private string[] ChartLabels { get; set; } = [];
+    private int Index { get; set; } = -1;
+    private ChartOptions ChartOptions { get; set; } = new ChartOptions();
+
     private EventMetric? EventMetric { get; set; }
 
     private Event? Event { get; set; }
@@ -20,5 +27,19 @@ public partial class EventMetrics
     {
         EventMetric = await EventService.GetEventMetricsAsync(EventId);
         Event = await EventService.GetEventAsync(EventId);
+
+        if (EventMetric != null)
+        {
+            ChartSeries =
+            [
+                new ChartSeries
+                {
+                    Name = "Requests",
+                    Data = EventMetric.ModelData.ChartData.Select(cd => (double)cd.Requests).ToArray()
+                }
+            ];
+
+            ChartLabels = EventMetric.ModelData.ChartData.Select(cd => cd.DateStamp.ToString("dd/MM/yyyy")).ToArray();
+        }
     }
 }
