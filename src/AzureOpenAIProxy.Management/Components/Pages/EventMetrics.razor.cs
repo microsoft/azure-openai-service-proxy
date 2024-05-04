eb7d40d63e7259ff0aa80dea5a5c780f6a9d5260
Dave Glover <dglover@microsoft.com>
2024-05-04 13:56:39 +1000

    scale chart labels

--- src/AzureOpenAIProxy.Management/Components/Pages/EventMetrics.razor.cs
+++ src/AzureOpenAIProxy.Management/Components/Pages/EventMetrics.razor.cs
@@ -25,21 +25,51 @@ public partial class EventMetrics
 
     protected override async Task OnInitializedAsync()
     {
+        DateTime? previousDay = null;
+        long previousRequests = 0;
+
         EventMetric = await EventService.GetEventMetricsAsync(EventId);
         Event = await EventService.GetEventAsync(EventId);
 
-        if (EventMetric?.ModelData?.ChartData is not null)
+        if (EventMetric?.ModelData?.ChartData != null)
         {
+            List<ChartData> cd = [];
+
+            foreach (var row in EventMetric.ModelData.ChartData.OrderBy(r => r.DateStamp))
+            {
+                if (previousDay != null)
+                {
+                    while (previousDay.Value.AddDays(1) < row.DateStamp)
+                    {
+                        cd.Add(new ChartData { DateStamp = previousDay.Value.AddDays(1), Requests = previousRequests });
+                        previousDay = previousDay.Value.AddDays(1);
+                    }
+                    cd.Add(row);
+                    previousDay = row.DateStamp;
+                    previousRequests = row.Requests;
+                }
+                else
+                {
+                    previousDay = row.DateStamp;
+                    previousRequests = row.Requests;
+                    cd.Add(row);
+                }
+            }
+
             ChartSeries =
             [
                 new ChartSeries
                 {
                     Name = "Requests",
-                    Data = EventMetric.ModelData.ChartData.Select(cd => (double)cd.Requests).ToArray()
+                    Data = cd.Select(cd => (double)cd.Requests).ToArray()
                 }
             ];
 
-            ChartLabels = EventMetric.ModelData.ChartData.Select(cd => cd.DateStamp.ToString("dd/MM")).ToArray();
+            ChartLabels = cd.Select(cd => cd.DateStamp.ToString("dd MMM")).ToArray();
+
+            // Scale the labels so they don't overlap. Allow for 10 labels max.
+            int chartLabelInterval = (ChartLabels.Length / 10) + 1;
+            ChartLabels = ChartLabels.Select((label, index) => index % chartLabelInterval == 0 ? label : "").ToArray();
         }
     }
 }
