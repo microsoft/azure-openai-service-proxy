84acfa4d6ca35230cf8064e139e63e78050e2597
Aaron Powell <me@aaron-powell.com>
2024-07-11 04:39:25 +0000

    starting work on using aspire for local dev

--- src/AzureAIProxy.AppHost/Program.cs
+++ src/AzureAIProxy.AppHost/Program.cs
@@ -1,3 +1,23 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var proxy = builder.AddProject<Projects.AzureAIProxy>("proxy");
+
+var playground = builder.AddNpmApp("playground", "../playground", scriptName: "dev")
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints();
+
+var admin = builder.AddProject<Projects.AzureAIProxy_Management>("admin")
+    .WithReference(playground)
+    .WithExternalHttpEndpoints();
+
+var swa = builder.AddExecutable("swa", "swa", Environment.CurrentDirectory)
+    .WithArgs(ctx =>
+    {
+        ctx.Args.Add("start");
+        ctx.Args.Add("--app-devserver-url");
+        ctx.Args.Add(playground.GetEndpoint("http").Url);
+        ctx.Args.Add("--api-devserver-url");
+        ctx.Args.Add(proxy.GetEndpoint("http").Url);
+    });
+
 builder.Build().Run();
