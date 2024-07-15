535a40a892a2a52e607f950e9917db45470e5b02
Dave Glover <dglover@microsoft.com>
2024-07-15 14:38:30 +1000

    Update AzureAISearch.cs

--- src/AzureAIProxy/Routes/AzureAISearch.cs
+++ src/AzureAIProxy/Routes/AzureAISearch.cs
@@ -47,31 +47,14 @@ public static class AzureAISearch
                 );
 
             var url = GenerateEndpointUrl(deployment, extPath, apiVersion);
-
-            try
-            {
-                var (responseContent, statusCode) = await proxyService.HttpPostAsync(
-                    url,
-                    deployment.EndpointKey,
-                    requestJsonDoc,
-                    requestContext,
-                    deployment
-                );
-                return new ProxyResult(responseContent, statusCode);
-
-            }
-            catch (TaskCanceledException ex) when (ex.InnerException is System.Net.Sockets.SocketException)
-            {
-                return OpenAIResult.ServiceUnavailable("The request was canceled due to timeout. Inner exception: " + ex.InnerException.Message);
-            }
-            catch (TaskCanceledException ex)
-            {
-                return OpenAIResult.ServiceUnavailable("The request was canceled: " + ex.Message);
-            }
-            catch (HttpRequestException ex)
-            {
-                return OpenAIResult.ServiceUnavailable("The request failed: " + ex.Message);
-            }
+            var (responseContent, statusCode) = await proxyService.HttpPostAsync(
+                url,
+                deployment.EndpointKey,
+                requestJsonDoc,
+                requestContext,
+                deployment
+            );
+            return new ProxyResult(responseContent, statusCode);
         }
     }
 
