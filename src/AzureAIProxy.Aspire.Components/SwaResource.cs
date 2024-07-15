d53095e6f51ee57cea20b143b4c82d43cddd14b5
Aaron Powell <me@aaron-powell.com>
2024-07-15 05:54:01 +0000

    Using a primary constructor

--- src/AzureAIProxy.Aspire.Components/SwaResource.cs
+++ src/AzureAIProxy.Aspire.Components/SwaResource.cs
@@ -1,14 +1,10 @@
 ﻿namespace AzureAIProxy.Aspire.Components;
 
-public class SwaResource : ExecutableResource
+public class SwaResource(string name, string workingDirectory) : ExecutableResource(name, "swa", workingDirectory)
 {
     internal IResourceBuilder<IResourceWithEndpoints>? AppResource { get; set; }
     internal IResourceBuilder<IResourceWithEndpoints>? ApiResource { get; set; }
 
     internal string? AppEndpoint => AppResource?.Resource.GetEndpoint("http").Url;
     internal string? ApiEndpoint => ApiResource?.Resource.GetEndpoint("http").Url;
-
-    public SwaResource(string name, string workingDirectory) : base(name, "swa", workingDirectory)
-    {
-    }
 }
